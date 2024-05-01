071890f79055ea42394b075b5c2930ff4189258b
harnoorjosan <91575135+harnoorjosan@users.noreply.github.com>
2024-04-30 19:41:43 -0600

    Text chunks
    Divide raw text into smaller chunks

--- app.py
+++ app.py
@@ -1,19 +1,24 @@
-import streamlit as st
-from dotenv import load_dotenv
-from PyPDF2 import PdfReader
+import streamlit as st # for frontend
+from dotenv import load_dotenv # for loading .env variables
+from PyPDF2 import PdfReader # for getting raw text from PDFs
+from langchain.text_splitter import CharacterTextSplitter # for getting chunks of texts from raw text
 
 def get_raw_text(docs):
     raw_text = ""
-    for doc in docs:
+    for doc in docs: # looping over each PDF
         pdf_reader = PdfReader(doc) # PdfReader can extract pages
         for page in pdf_reader.pages: # looping over each page
             raw_text += page.extract_text()
     return raw_text
 
+def get_text_chunks(raw_text):
+    text_splitter = CharacterTextSplitter(separator="\n", chunk_size = 1000, chunk_overlap = 200, length_function = len)
+    chunks = text_splitter.split_text(raw_text)
+    return chunks
 
 
 def main():
-    load_dotenv()
+    load_dotenv() # loading .env variables
     st.set_page_config(page_title="Chat with PDFs", page_icon=":books:") # ":xxxx:" is used for emojis
     st.header("Chat with PDFs :books:")
     st.text_input("Type your question here....")
@@ -25,9 +30,10 @@ def main():
             with st.spinner("Processing....."): # a spinner for processing
                 # extract raw text from pdf
                 raw_text = get_raw_text(docs)
-                st.write(raw_text)
                 
                 # divide raw text into chunks
+                chunks = get_text_chunks(raw_text)
+                #st.write(chunks)
                 
                 # store in vector database
 if __name__ == '__main__':
