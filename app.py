a5bf8d6623fd50eff85d2439553c88e40175b10a
harnoorjosan <91575135+harnoorjosan@users.noreply.github.com>
2024-04-30 18:30:02 -0600

    Extract Raw Text
    1. Extract raw text from PDFs
    2. Show spinner when processing
    3. Allow uploading multiple PDFs

--- app.py
+++ app.py
@@ -1,5 +1,16 @@
 import streamlit as st
 from dotenv import load_dotenv
+from PyPDF2 import PdfReader
+
+def get_raw_text(docs):
+    raw_text = ""
+    for doc in docs:
+        pdf_reader = PdfReader(doc) # PdfReader can extract pages
+        for page in pdf_reader.pages: # looping over each page
+            raw_text += page.extract_text()
+    return raw_text
+
+
 
 def main():
     load_dotenv()
@@ -8,9 +19,17 @@ def main():
     st.text_input("Type your question here....")
     with st.sidebar:
         st.subheader("Your PDFs")
-        st.file_uploader("Upload PDFs here")
-        st.button("Process")
-
+        docs = st.file_uploader("Upload PDFs here", accept_multiple_files=True)  # accept multiple docs
+        button = st.button("Process")
+        if button: # button pressed
+            with st.spinner("Processing....."): # a spinner for processing
+                # extract raw text from pdf
+                raw_text = get_raw_text(docs)
+                st.write(raw_text)
+                
+                # divide raw text into chunks
+                
+                # store in vector database
 if __name__ == '__main__':
     main()
     
