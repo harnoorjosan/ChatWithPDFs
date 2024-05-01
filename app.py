8c87c2b05baa3720f09855b1b9a336ff23ca2e3a
harnoorjosan <91575135+harnoorjosan@users.noreply.github.com>
2024-05-01 12:40:41 -0600

    Issues with LLM
    Some issues persistent in LLM.

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ from langchain_community.embeddings import HuggingFaceInstructEmbeddings
 #from langchain.embeddings import 
 from langchain_community.vectorstores import FAISS
 import torch
+from langchain.memory import ConversationBufferMemory
+from langchain.chains.conversational_retrieval.base import ConversationalRetrievalChain
+from langchain.chat_models.openai import ChatOpenAI
+from langchain_community.chat_models.huggingface import ChatHuggingFace
 
 def get_raw_text(docs):
     raw_text = ""
@@ -26,11 +30,22 @@ def get_vector_store(chunks):
     vector_store = FAISS.from_texts(texts=chunks, embedding=embeddings)
     return vector_store
 
+'''def get_convo_chain(vector_store):
+    llm = ChatHuggingFace()
+    memory = ConversationBufferMemory(memory_key="chat history", return_messages=True)
+    convo_chain = ConversationalRetrievalChain.from_llm(llm=llm, retriever=vector_store.as_retriever(), memory=memory)
+    return convo_chain'''
+
+
 def main():
     load_dotenv() # loading .env variables
     st.set_page_config(page_title="Chat with PDFs", page_icon=":books:") # ":xxxx:" is used for emojis
     st.header("Chat with PDFs :books:")
     st.text_input("Type your question here....")
+
+    if "conversation" not in st.session_state:
+        st.session_state.convo_chain =  None
+
     with st.sidebar:
         st.subheader("Your PDFs")
         docs = st.file_uploader("Upload PDFs here", accept_multiple_files=True)  # accept multiple docs
@@ -40,6 +55,10 @@ def main():
                 raw_text = get_raw_text(docs) # extract raw text from pdf
                 chunks = get_text_chunks(raw_text) # divide raw text into chunks
                 vector_store = get_vector_store(chunks)# store in vector database
+                #st.session_state will bind the convo to chat therefore will stay persistent. Streamlit otw loads the code whenever there is an action and the convo will lose
+                #st.session_state.convo_chain = get_convo_chain(vector_store)# start a conversation chain
+
+
 if __name__ == '__main__':
     main()
     
